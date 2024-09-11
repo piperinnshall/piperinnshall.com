f131089dc5e5ebd499d0de0a0ae6683da414505b
piperinnshall <piper.innshall@gmail.com>
2024-09-11 13:39:48 +1200

    chore(styles): style footer and nav bar

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,3 @@
 document.querySelector('.hamburger').addEventListener('click', () => {
-      document.querySelector('.navigation .nav-links-wrapper').classList.toggle('expanded');
+      document.querySelector('nav .nav-links-wrapper').classList.toggle('expanded');
 });
