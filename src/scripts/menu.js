b3a754c825bfe3b63b51f62116ff6aa2a6a900ec
Piper Inns Hall <piper.innshall@gmail.com>
2025-12-07 00:59:11 +1300

    feat: Add pdf viewer

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,9 @@
 document.querySelector('.hamburger').addEventListener('click', () => {
-      document.querySelector('nav .nav-links-wrapper').classList.toggle('expanded');
+  document.querySelector('nav .nav-links-wrapper').classList.toggle('expanded');
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const card = document.querySelector('.card');
+  if(card) card.classList.add('loaded');
+});
+
